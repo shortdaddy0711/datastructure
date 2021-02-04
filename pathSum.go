799f2154ac9ec968461230ed261fb01ab452d30e
shortdaddy0711 <sodoatm@gmail.com>
2021-02-03 21:36:03 -0800

    completed path sum

--- pathSum.go
+++ pathSum.go
@@ -5,59 +5,46 @@ import (
 	"fmt"
 )
 
-type binaryTree struct {
-	val   int
-	left  *binaryTree
-	right *binaryTree
+type treeNode struct {
+	Val   int       `json:"val"`
+	Left  *treeNode `json:"left"`
+	Right *treeNode `json:"right"`
 }
 
-// func targetMatch(root binaryTree, target int) bool {
-
-// }
+func hasPathSum(root *treeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val == sum {
+			return true
+		}
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
 
 func main() {
 	root := []byte(`{
-		val: 5,
-		left: {
-			val: 4,
-			left: {
-				val:   11,
-				left:  {val: 7, left: null, right: null},
-				right: {val: 2, left: null, right: null},
-			},
-			right: null,
-		},
-		right: {
-			val:   8,
-			left:  {val: 13, left: null, right: null},
-			right: {val: 4, left: null, right: {val: 1, left: null, right: null}},
-		}}`)
-	var tree binaryTree
+		"val": 5,
+		"left": {
+			"val": 4,
+			"left": {
+				"val":   11,
+				"left":  {"val": 7, "left": null, "right": null},
+				"right": {"val": 2, "left": null, "right": null}},
+			"right": null},
+		"right": {
+			"val":   8,
+			"left":  {"val": 13, "left": null, "right": null},
+			"right": {"val": 4, "left": null, "right": {"val": 1, "left": null, "right": null}}}}`)
+	var tree treeNode
 	err := json.Unmarshal(root, &tree)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%v\n", tree)
+	fmt.Printf("%#v\n", hasPathSum(&tree, 22))
+	fmt.Printf("%#v\n", hasPathSum(&tree, 27))
+	fmt.Printf("%#v\n", hasPathSum(&tree, 26))
+	fmt.Printf("%#v\n", hasPathSum(&tree, 18))
+	fmt.Printf("%#v\n", hasPathSum(&tree, 19))
 }
-
-// const pathSum = (root, target) => {
-//   if (!root) {
-//     return false;
-//   }
-//   let isTargetExist = false;
-//   function checkLeaf(node, sum) {
-//     if (!node) {
-//       return;
-//     } else if (node.right === null && node.left === null) {
-//       if (target === sum + node.val) {
-//         isTargetExist = true;
-//       }
-//       return;
-//     } else {
-//       checkLeaf(node.left, sum + node.val);
-//       checkLeaf(node.right, sum + node.val);
-//     }
-//   }
-//   checkLeaf(root, 0);
-//   return isTargetExist;
-// };
